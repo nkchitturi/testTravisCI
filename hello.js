549e2ba72d13bfa187cf8fa5860264e5848b22d0
nkchitturi <kiran.chitturi@sungardas.com>
2016-08-25 12:59:29 -0500

    updating hello.js & testing gitignore check in

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 // var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Travis! How is it going ?\n'); // fixed the failing Travis CI Lint
+  res.end('Hello T-CI! How is it going ?\n'); // fixed the failing Travis CI Lint
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
